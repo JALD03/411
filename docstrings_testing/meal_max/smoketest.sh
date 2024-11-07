14620cc37ba182d8098b24b3a4c2709323c48dc3
Anna LaPrade <annalaprade@Reginald.local>
2024-11-07 12:57:17 -0500

    some of anna's smoketests

--- docstrings_testing/meal_max/smoketest.sh
+++ docstrings_testing/meal_max/smoketest.sh
@@ -46,6 +46,130 @@ check_db() {
   fi
 }
 
+
+# Meal Management
+
+# create_meal 
+
+create_meal() {
+  name=$1
+  cuisine=$2
+  price=$3
+  dificulty=$4
+
+  echo "Adding meal ($meal - $cuisine, $price) to the database..."
+  curl -s -X POST "$BASE_URL/create-meal" -H "Content-Type: application/json" \
+    -d "{\"name\":\"$name\", \"cuisine\":\"$cuisine\", \"price\":$price, \"difficulty\":\"$difficulty\"}" | grep -q '"status": "success"'
+
+
+  # if added, then it will exit with a 0
+  if [ $? -eq 0 ]; then
+    echo "Meal added successfully."
+  else
+    echo "Failed to add meal."
+    exit 1
+  fi
+}
+
+
+clear_meals() {
+  echo "Clearing meals from database..."
+
+  curl -s -X POST "$BASE_URL/clear-meals" | grep -q '"status": "success"'
+
+}
+
+
+delete_meal() {
+  meal_id=$1
+
+  echo "Deleting meal by ID ($meal_id)..."
+
+  response=$(curl -s -X DELETE "$BASE_URL/delete-meal/$meal_id")
+  if echo "$response" | grep -q '"status": "success"'; then
+    echo "Meal deleted successfully by ID ($meal_id)."
+  else
+    echo "Failed to delete meal by ID ($meal_id)."
+    exit 1
+  fi
+
+}
+
+
+get_leaderboard() {
+  sort_by = $1 
+
+  echo "Retreiving leaderboard sorted by $sort_by..."
+
+  response=$(curl -s -G "$BASE_URL/get-leaderboard" --data-urlencode "sort_by=$sort_by")
+
+  if echo "$response" | grep -q '"status": "success"'; then
+    echo "Leaderboard retrieved successfully:"
+  
+  else
+    echo "Failed to retrieve leaderboard. Response:"
+    exit 1
+  fi
+
+}
+
+
+get_meal_by_id() {
+  meal_id=$1
+
+  echo "Retrieving meal by ID ($meal_id)..."
+
+  response=$(curl -s -G "$BASE_URL/get-meal-by-id" --data-urlencode "meal_id=$meal_id")
+
+  if echo "$response" | grep -q '"status": "success"'; then
+    echo "Meal retrieved successfully by ID ($meal_id)."
+    if [ "$ECHO_JSON" = true ]; then
+      echo "Meal JSON (ID $meal_id):"
+      echo "$response" | jq .
+    fi
+  else
+    echo "Failed to get meal by ID ($meal_id)."
+    exit 1
+  fi
+}
+
+
+get_meal_by_name() {
+  meal_name=$1
+
+  echo "Getting meal by name ($meal_name)..."
+  response=$(curl -s -X GET "$BASE_URL/get-meal-by-name/$meal_name")
+
+  if echo "$response" | grep -q '"status": "success"'; then
+    echo "Meal retrieved successfully by name ($meal_name)."
+    if [ "$ECHO_JSON" = true ]; then
+      echo "Meal JSON (Name $meal_name):"
+      echo "$response" | jq .
+    fi
+  else
+    echo "Failed to get meal by name ($meal_name)."
+    exit 1
+  fi
+}
+
+
+update_meal_stats() {
+  meal_id=$1
+  result=$2
+
+  echo "Updating meal stats for meal ID ($meal_id) with result ($result)..."
+
+  response=$(curl -s -X PUT "$BASE_URL/update-meal-stats/$meal_id" -d "{\"result\": \"$result\"}")
+
+  if echo "$response" | grep -q '"status": "success"'; then
+    echo "Meal stats updated successfully for meal ID ($meal_id)."
+  else
+    echo "Failed to update meal stats for meal ID ($meal_id)."
+    exit 1
+  fi
+}
+
+
 ##########################################################
 #
 # Battle Management
